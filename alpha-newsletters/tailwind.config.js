0f721872ded5df2f5993459f90f6c75bbc4f3e1f
jordan <i.aslanoglou@gazzetta.gr>
2025-04-30 15:55:56 +0300

    alpha gold / hero image

--- alpha-newsletters/tailwind.config.js
+++ alpha-newsletters/tailwind.config.js
@@ -1,5 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  theme: {
+    extend:{
+      fontFamily: {
+        sans: ['Arial', 'sans-serif'],
+      }
+    },
+    screens: {
+      sm: {max: '650px'},
+      xs: {max: '425px'},
+    }
+  },
   presets: [
     require('tailwindcss-preset-email'),
   ],
